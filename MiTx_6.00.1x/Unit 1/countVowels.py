1e6175a1c320cee3945f9de6e50de4ddfa5073c3
naxdyn <danny.wang@torontomu.ca>
2023-08-02 04:01:09 -0400

    Changed formatting

--- MiTx_6.00.1x/Unit 1/countVowels.py
+++ MiTx_6.00.1x/Unit 1/countVowels.py
@@ -5,5 +5,5 @@ s = 'azcbobobegghakl'
 count = 0
 for char in s:
     if char=='a' or char=='e' or char=='i' or char=='o' or char=='u':
-        count+=1
+        count += 1
 print("Number of vowels: " + str(count))
