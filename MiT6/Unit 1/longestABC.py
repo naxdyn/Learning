dcc2742951b3891e9c13bc25eabb00ca89f3aafb
naxdyn <danny.wang@torontomu.ca>
2023-09-09 23:50:23 -0400

    made some progress

--- MiT6/Unit 1/longestABC.py
+++ MiT6/Unit 1/longestABC.py
@@ -1,1 +1,18 @@
 # Prints the longest substring that is in alphabetical order
+
+s = 'azcbobobegghakl'
+
+longest = ""
+
+if len(s) == 0:
+    print("error")
+else:
+    longest = s[0]
+    for i in range(len(s)):
+        # checks if the current longest string is longer than the remainder of the string left to check to end early
+        if len(longest) > len(s[i:]):
+            break
+    
+    
+
+
